fe73f4ddeda7f72a5846c3967be9386efaed140a
meyzaal <almayza17@gmail.com>
2024-01-15 15:56:36 +0700

    refactor: Update kg client configuration

--- packages/kg_client/lib/kg_client.dart
+++ packages/kg_client/lib/kg_client.dart
@@ -1,4 +1,10 @@
-/// A Very Good Project created by Very Good CLI.
+/// Kampus Gratis Client Library
+///
+/// A Dart library for interacting with the KG API, providing a client for
+/// making HTTP requests, token management, and support for different
+/// application flavors.
 library kg_client;
 
+export 'src/failure/failure.dart';
 export 'src/kg_client.dart';
+export 'src/models/token.dart';
--- packages/kg_client/lib/src/kg_client.dart
+++ packages/kg_client/lib/src/kg_client.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:fresh_dio/fresh_dio.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:kg_client/src/failure/failure.dart';
 import 'package:kg_client/src/local_storage.dart';
 import 'package:kg_client/src/models/token.dart';
 import 'package:ua_client_hints/ua_client_hints.dart';
@@ -19,52 +21,68 @@ enum KgFlavor {
   production,
 }
 
-/// A client for interacting with the Kg API.
-///
-/// The [KgClient] provides methods for making HTTP requests to the Kg API,
-/// handling token authentication, and supporting different application flavors.
+/// A Dart class representing the KgClient, which is responsible for managing
+/// HTTP requests to the Kg API. It can be configured with a custom Dio HTTP
+/// client and KgFlavor.
 class KgClient {
-  /// Private constructor for initializing the [KgClient].
+  /// Constructs a KgClient instance.
   ///
-  /// The constructor takes an [httpClient] and [flavor], and initializes
-  /// interceptors, including token management, logging, and user agent headers.
-  KgClient._({
-    required Dio httpClient,
-    required KgFlavor flavor,
-  }) : _httpClient = httpClient {
-    _initializeInterceptors(baseUrl: flavor.baseUrl);
-  }
-
-  /// Public constructor for creating an instance of [KgClient].
-  ///
-  /// Optionally, you can provide a custom [httpClient] or it will create a new
-  /// one.
-  KgClient.init({
-    required KgFlavor flavor,
+  /// Parameters:
+  /// - [httpClient]: Optional Dio HTTP client. If not provided, a default
+  ///   instance will be created.
+  /// - [flavor]: Optional KgFlavor specifying the environment flavor. If not
+  ///   provided, the default is KgFlavor.development.
+  KgClient({
     Dio? httpClient,
-  }) : this._(httpClient: httpClient ?? Dio(), flavor: flavor);
+    KgFlavor? flavor,
+  }) : _httpClient = (httpClient ?? Dio())
+          ..options.baseUrl = (flavor ?? KgFlavor.development).baseUrl
+          ..interceptors.add(fresh)
+          ..interceptors.add(
+            LogInterceptor(requestBody: true, responseBody: true),
+          )
+          ..interceptors.add(
+            InterceptorsWrapper(
+              onRequest: (options, handler) async {
+                if (Platform.isAndroid || Platform.isIOS) {
+                  options.headers.addAll(await userAgentClientHintsHeader());
+                }
 
-  /// Initializes HTTP request interceptors including token management,
-  /// logging, and user agent headers.
-  void _initializeInterceptors({required String baseUrl}) {
-    _httpClient
-      ..options.baseUrl = baseUrl
-      ..interceptors.add(fresh)
-      ..interceptors.add(
-        LogInterceptor(requestBody: true, responseBody: true),
-      )
-      ..interceptors.add(
-        InterceptorsWrapper(
-          onRequest: (options, handler) async {
-            if (Platform.isAndroid || Platform.isIOS) {
-              options.headers.addAll(await userAgentClientHintsHeader());
-            }
+                return handler.next(options);
+              },
+              onError: (error, handler) {
+                final response = error.response as Response<JSON>?;
 
-            return handler.next(options);
-          },
-        ),
-      );
-  }
+                if (error.type == DioExceptionType.badResponse) {
+                  throw BadResponseFailure(
+                    message: response?.data?['message'] as String?,
+                    statusCode: response?.statusCode,
+                  );
+                }
+
+                if (error.type == DioExceptionType.connectionTimeout) {
+                  throw ConnectionTimeoutFailure();
+                }
+
+                if (error.type == DioExceptionType.connectionError ||
+                    error.type == DioExceptionType.receiveTimeout ||
+                    error.type == DioExceptionType.sendTimeout) {
+                  throw ConnectionErrorFailure();
+                }
+
+                if (error.type == DioExceptionType.unknown) {
+                  throw UnknownFailure();
+                }
+
+                return handler.next(error);
+              },
+            ),
+          );
+
+  /// Initializes Hive for Flutter.
+  ///
+  /// This method must be called before using [KgClient] for http request.
+  Future<void> initializeHive() => Hive.initFlutter();
 
   /// Dio instance for making HTTP requests.
   final Dio _httpClient;
@@ -74,93 +92,77 @@ class KgClient {
   /// The [fresh] instance is configured with a [LocalStorage] for token
   /// storage, a refresh token function, a token header function, and a should
   /// refresh function. It allows for automatic token refreshing when needed.
-  final Fresh<Token> fresh = Fresh<Token>(
+  static final Fresh<Token> fresh = Fresh<Token>(
     tokenStorage: LocalStorage<Token>(_tokenBoxName),
-    refreshToken: _refreshToken,
-    tokenHeader: _tokenHeader,
-    shouldRefresh: _shouldRefresh,
+    refreshToken: (token, httpClient) async {
+      debugPrint('refreshing token...');
+
+      final refreshToken = token?.refreshToken;
+      if (refreshToken == null) throw RevokeTokenException();
+
+      try {
+        final requestBody = {'refresh_token': refreshToken};
+        final response = await httpClient.post<JSON>('/api/v1/auth/refresh',
+            data: requestBody);
+
+        if (response.data == null) throw RevokeTokenException();
+
+        debugPrint('token refreshed!');
+        final result = Token.fromJson(response.data?['data'] as JSON);
+        return Token(
+          accessToken: result.accessToken,
+          refreshToken: result.refreshToken,
+        );
+      } catch (_) {
+        throw RevokeTokenException();
+      }
+    },
+    tokenHeader: (token) {
+      return {'authorization': '${token.tokenType} ${token.accessToken}'};
+    },
+    shouldRefresh: (response) {
+      return response?.statusCode == 401;
+    },
   );
 
-  /// Determines whether a token refresh is needed based on the HTTP response.
+  /// Returns the underlying Dio HTTP client instance without any additional
+  /// modifications.
   ///
-  /// This function checks if the HTTP response status code is 401
-  /// (Unauthorized), indicating that the current token is invalid. If true, a
-  /// token refresh is triggered.
-  static bool _shouldRefresh(Response<dynamic>? response) {
-    return response?.statusCode == 401;
-  }
+  /// Use this client for making regular HTTP requests without authentication.
+  Dio get client => _httpClient;
 
-  /// Refreshes the authentication token using a refresh token.
-  static Future<Token> _refreshToken(Token? token, Dio client) async {
-    debugPrint('refreshing token...');
+  /// Returns an authorized instance of the Dio HTTP client with interceptors
+  /// for handling authentication tokens.
+  ///
+  /// The Dio instance is enhanced with an interceptor that adds authorization
+  /// headers to the outgoing requests based on the authentication token
+  /// obtained from the [fresh] object. If a valid token is present, the
+  /// 'authorization' header is added with the token type and access token. If
+  /// no token is available, the request proceeds without modification.
+  ///
+  /// Returns:
+  /// - An authorized Dio HTTP client with the necessary interceptors for
+  ///   handling authentication.
+  Dio get authorizedClient {
+    return _httpClient
+      ..interceptors.add(
+        InterceptorsWrapper(
+          onRequest: (options, handler) async {
+            // Retrieve the authentication token from the [fresh] object.
+            final token = await fresh.token;
 
-    final refreshToken = token?.refreshToken;
-    if (refreshToken == null) throw RevokeTokenException();
+            // Add authorization headers if a valid token is present.
+            if (token != null) {
+              options.headers.addAll(
+                {'authorization': '${token.tokenType} ${token.accessToken}'},
+              );
+            }
 
-    try {
-      final requestBody = {'refresh_token': refreshToken};
-      final response =
-          await client.post<JSON>('/api/v1/auth/refresh', data: requestBody);
-
-      if (response.data == null) throw RevokeTokenException();
-
-      debugPrint('token refreshed!');
-      final result = Token.fromJson(response.data?['data'] as JSON);
-      return Token(
-        accessToken: result.accessToken,
-        refreshToken: result.refreshToken,
+            return handler.next(options);
+          },
+        ),
       );
-    } catch (_) {
-      throw RevokeTokenException();
-    }
   }
-
-  /// Generates headers containing the authentication token.
-  static Map<String, String> _tokenHeader(Token token) {
-    return {'authorization': '${token.tokenType} ${token.accessToken}'};
-  }
-
-  /// A utility function for authenticating a user with access and refresh 
-  /// tokens.
-  ///
-  /// This function sets the provided access and refresh tokens in the token 
-  /// storage using the Fresh package. It creates a new [Token] instance and 
-  /// updates the token storage with the user's authentication information.
-  ///
-  /// Parameters:
-  /// - [accessToken]: The access token received after user authentication.
-  /// - [refreshToken]: The refresh token used for refreshing the access token.
-  ///
-  /// Example usage:
-  /// ```dart
-  /// await authenticate(
-  ///   accessToken: 'sampleAccessToken',
-  ///   refreshToken: 'sampleRefreshToken',
-  /// );
-  /// ```
-  Future<void> authenticate({
-    required String accessToken,
-    required String refreshToken,
-  }) {
-    return fresh.setToken(
-      Token(
-        accessToken: accessToken,
-        refreshToken: refreshToken,
-      ),
-    );
-  }
-
-  /// A utility function for unauthenticating a user.
-  ///
-  /// This function clears the authentication tokens in the token storage
-  /// by setting the token to null using the Fresh package. It effectively
-  /// logs out the user by removing their authentication information.
-  ///
-  /// Example usage:
-  /// ```dart
-  /// await unauthenticate();
-  /// ```
-  Future<void> unauthenticate() => fresh.setToken(null);
 }
 
 /// JSON type definition for Map<String, dynamic>.
